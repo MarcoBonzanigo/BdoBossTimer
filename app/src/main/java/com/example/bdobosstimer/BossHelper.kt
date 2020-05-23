83e6c56f0f81c201789fdb49449527582fe57bc5
MarcoBonzanigo <marcobonzanigo@gmail.com>
2020-05-23 12:42:23 +0200

    Base Implementation

--- app/src/main/java/com/example/bdobosstimer/BossHelper.kt
+++ app/src/main/java/com/example/bdobosstimer/BossHelper.kt
@@ -5,25 +5,35 @@ import kotlin.math.ceil
 
 val timeGrid = arrayOf("00:15","02:00","05:00","09:00","12:00","16:00","19:00","22:15","23:15");
 val timeIntGrid = arrayOf(25,200,500,900,1200,1600,1900,2225,2325)
+const val kzarka = "Kzarka"
+const val karanda = "Karanda"
+const val nouver = "Nouver"
+const val kutum = "Kutum"
+const val garmoth = "Garmoth"
+const val offin = "Offin"
+const val vell = "Vell"
+const val quint = "Quint"
+const val muraka = "Muraka"
+const val empty = ""
 val bossGrid = arrayOf(
-    arrayOf("Karanda&Kutum","Karanda","Kzarka","Kzarka","Offin","Kutum","Nouver","Kzarka",""),
-    arrayOf("Karanda","Kutum","Kzarka","Nouver","Kutum","Nouver","Karanda","Garmoth",""),
-    arrayOf("Kutum&Kzarka","Karanda","Kzarka","Karanda","","Kutum","Offin","Karanda&Kzarka","Quint&Muraka"),
-    arrayOf("Nouver","Kutum","Nouver","Kutum","Nouver","Kzarka","Kutum","Garmoth",""),
-    arrayOf("Kzarka&Karanda","Nouver","Karanda","Kutum","Karanda","Nouver","Kzarka","Kutum&Kzarka",""),
-    arrayOf("Karanda","Offin","Nouver","Kutum","Nouver","Quint&Muaraka","Karanda&Kzarka","",""),
-    arrayOf("Nouver&Kutum","Kzarka","Kutum","Nouver","Kzarka","Vell","Garmoth","Kzarka&Nouver","")
+    arrayOf("$karanda&$kutum", karanda, kzarka, kzarka, offin, kutum, nouver, kzarka, empty),
+    arrayOf(karanda, kutum, kzarka, nouver, kutum, nouver, karanda, garmoth, empty),
+    arrayOf("$kutum&$kzarka", karanda, kzarka, karanda, empty, kutum, offin,"$karanda&$kzarka","$quint&$muraka"),
+    arrayOf(nouver, kutum, nouver, kutum, nouver, kzarka, kutum, garmoth, empty),
+    arrayOf("$kzarka&$karanda", nouver, karanda, kutum, karanda, nouver, kzarka,"$kutum&$kzarka",empty),
+    arrayOf(karanda, offin, nouver, kutum, nouver,"$quint&$muraka","$karanda&$kzarka",empty,empty),
+    arrayOf("$nouver&$kutum", kzarka, kutum, nouver, kzarka, vell, garmoth,"$kzarka&$nouver",empty)
 )
 val imageMap = hashMapOf<String,Int>(
-    "Garmoth" to R.drawable.garmoth_big,
-    "Karanda" to R.drawable.karanda_big,
-    "Kutum" to R.drawable.kutum_big,
-    "Kzarka" to R.drawable.kzarka_big,
-    "Offin" to R.drawable.offin_big,
-    "Nouver" to R.drawable.nouver_big,
-    "Quint" to R.drawable.quint_big,
-    "Muraka" to  R.drawable.muraka_big,
-    "Vell" to  R.drawable.vell_big
+    garmoth to R.drawable.garmoth_big,
+    karanda to R.drawable.karanda_big,
+    kutum to R.drawable.kutum_big,
+    kzarka to R.drawable.kzarka_big,
+    offin to R.drawable.offin_big,
+    nouver to R.drawable.nouver_big,
+    quint to R.drawable.quint_big,
+    muraka to  R.drawable.muraka_big,
+    vell to  R.drawable.vell_big
 )
 
 class BossHelper private constructor() {
@@ -54,7 +64,7 @@ class BossHelper private constructor() {
      fun getNextBoss(): Boss{
          val now = getTimeOfDay()
          for ((pointer, time) in timeIntGrid.withIndex()){
-            if (time > now && bossGrid[getDayOfTheWeek()][pointer] != ""){
+            if (time > now && bossGrid[getDayOfTheWeek()][pointer] != empty){
                 return resolveBoss(time, now, pointer, getDayOfTheWeek())
             }
          }
--- app/src/main/java/com/example/bdobosstimer/MainActivity.kt
+++ app/src/main/java/com/example/bdobosstimer/MainActivity.kt
@@ -1,20 +1,50 @@
 package com.example.bdobosstimer
 
+import android.app.NotificationManager
 import android.os.AsyncTask
 import android.os.Bundle
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import com.example.bdobosstimer.MainActivity.BossRefresher
 import kotlinx.android.synthetic.main.activity_main.*
 
 
+private var refresher: BossRefresher? = null
 class MainActivity : AppCompatActivity(), SynchronizedActivity{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onPause() {
+        cancelRefresher()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        cancelRefresher()
+        super.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
         synchronize()
     }
+
+    private fun cancelRefresher() {
+        refresher?.cancel(true)
+        refresher = null
+    }
+
+    override fun synchronize() {
+        updateBoss()
+        refresher = BossRefresher(this)
+        refresher!!.execute()
+    }
+
 
     private fun updateBoss() {
         val nextBoss = BossHelper.instance.getNextBoss()
@@ -31,11 +61,6 @@ class MainActivity : AppCompatActivity(), SynchronizedActivity{
         } else {
             main_image_boss_two.visibility = GONE
         }
-    }
-
-    override fun synchronize() {
-        updateBoss()
-        BossRefresher(this).execute()
     }
 
     class BossRefresher(private val synchronizedActivity: SynchronizedActivity) : AsyncTask<Void, Void, Void>() {
